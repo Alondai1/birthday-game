244da8bf665cd933a91151a77ed87b2f2ded755f
Alondai1 <alondai1@gmail.com>
2025-01-24 19:29:00 +0200

    PWA

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'birthday-game-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/game.js',
+    '/gameData.js',
+    '/manifest.json',
+    '/assets/images/profile.png',
+    '/assets/sounds/correct.mp3',
+    '/assets/sounds/incorrect.mp3'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        // Don't cache if not a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    });
+            })
+    );
+}); 
